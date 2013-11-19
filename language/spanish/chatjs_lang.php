78d56cfe6a926d4317c2b85b23f6df9af5005bb9
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2013-11-19 15:15:29 -0500

    Working Admin interface

--- language/spanish/chatjs_lang.php
+++ language/spanish/chatjs_lang.php
@@ -1,2 +1,5 @@
 <?php
 $lang['chatjs:chat'] = 'Chat';
+$lang['chatjs:activeUsers'] = 'Usuarios Activos';
+$lang['chatjs:bannedUsers'] = 'Usuarios Bloqueados';
+$lang['chatjs:bannedIPs'] = 'IP\'S Bloqueadas';
---
+++ views/admin/body.php
@@ -0,0 +1,37 @@
+<section class="title">
+    <h4>Chat JS</h4>
+</section>
+
+<section class="item">
+    <div class="content">
+        <div class="one_half">
+            <div class="tabs">
+                <ul class="tab-menu">
+                    <li><a href="#panel1"><span><?php echo lang('chatjs:activeUsers') ?></span></a></li>
+                    <li><a href="#panel2"><span><?php echo lang('chatjs:bannedUsers') ?></span></a></li>
+                    <li><a href="#panel3"><span><?php echo lang('chatjs:bannedIPs') ?></span></a></li>
+                </ul>
+                <div id="panel1">
+                    <ul class="ChatjsUsers">
+                        <li>
+                            <img src="http://lorempixel.com/50/50/" />
+                            <p>
+                                Jose Luis Fonseca <br />
+                                Última actividad : 10:30
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <div id="panel2">
+                    
+                </div>
+                <div id="panel3">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="one_half">
+            ...
+        </div>
+    </div>
+</section>
